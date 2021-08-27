83b96789fc713c02f92a9dc35477b11e908abd70
Jesus Gonzalez <angelotr12@hotmail.com>
2021-08-26 19:35:06 -0500

    Fix: Preview de imaganes
    Al resolver el problema de los previews del dropzone se dejan de ver la imaganes que se llaman por el metodo getFirstMediaUrl, con este fix ahora las imagenes se pueden visualizar

--- src/Views/includes/navbar.blade.php
+++ src/Views/includes/navbar.blade.php
@@ -22,7 +22,7 @@
             <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               <div class="media align-items-center">
                 <span class="avatar avatar-sm rounded-circle">
-                  <img alt="Image placeholder" src="{{Auth::guard('web')->user()->getFirstMediaUrl('users')}}">
+                  <img alt="Image placeholder" src="{{ asset('') }}{{Auth::guard('web')->user()->getFirstMediaUrl('users')}}">
                 </span>
                 <div class="media-body  ml-2  d-none d-lg-block">
                   <span class="mb-0 text-sm  font-weight-bold">{{ Auth::guard('web')->user()->name }}</span>
